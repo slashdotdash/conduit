e62bd8934532a9c90b6a312c0bddb17e00593911
Ben Smith <ben@10consulting.com>
2017-06-12 12:02:56 +0100

    Register user command property validation

---
+++ lib/conduit/validation/uuid_validator.ex
@@ -0,0 +1,14 @@
+defmodule Conduit.Validation.UuidValidator do
+  use Vex.Validator
+
+  def validate(value, _options) do
+    Vex.Validators.By.validate(value, [function: &valid_uuid?/1, allow_nil: false, allow_blank: false])
+  end
+
+  defp valid_uuid?(uuid) do
+    case UUID.info(uuid) do
+      {:ok, _} -> true
+      {:error, _} -> false
+    end
+  end
+end
