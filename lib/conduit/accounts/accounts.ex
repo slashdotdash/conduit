3397acf0bf09c7c16f0526e708c672a42d192b9a
Ben Smith <ben@10consulting.com>
2017-06-21 11:40:32 +0100

    Additional username validation

--- lib/conduit/accounts/accounts.ex
+++ lib/conduit/accounts/accounts.ex
@@ -17,8 +17,9 @@ defmodule Conduit.Accounts do
 
     register_user =
       attrs
-      |> assign(:user_uuid, uuid)
       |> RegisterUser.new()
+      |> RegisterUser.assign_uuid(uuid)
+      |> RegisterUser.downcase_username()
 
     with :ok <- Router.dispatch(register_user, consistency: :strong) do
       get(User, uuid)
@@ -43,6 +44,4 @@ defmodule Conduit.Accounts do
       projection -> {:ok, projection}
     end
   end
-
-  defp assign(attrs, key, value), do: Map.put(attrs, key, value)
 end
