5545e9b7a41eb7258549c245bd60b7df928f8c25
Ben Smith <ben@10consulting.com>
2017-06-29 10:40:35 +0100

    Scaffold blog articles

---
+++ lib/conduit/blog/projections/article.ex
@@ -0,0 +1,21 @@
+defmodule Conduit.Blog.Projections.Article do
+  use Ecto.Schema
+
+  @primary_key {:uuid, :binary_id, autogenerate: false}
+
+  schema "blog_articles" do
+    field :slug, :string
+    field :title, :string
+    field :description, :string
+    field :body, :string
+    field :tags, {:array, :string}
+    field :favorite_count, :integer
+    field :published_at, :naive_datetime
+    field :author_uuid, :binary_id
+    field :author_bio, :string
+    field :author_image, :string
+    field :author_username, :string
+
+    timestamps()
+  end
+end
