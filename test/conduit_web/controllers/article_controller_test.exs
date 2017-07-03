fd2e993db51c23ef82f3ef1039663fa0e097b5d6
Ben Smith <ben@10consulting.com>
2017-07-03 16:34:33 +0100

    List articles controller

--- test/conduit_web/controllers/article_controller_test.exs
+++ test/conduit_web/controllers/article_controller_test.exs
@@ -34,4 +34,77 @@ defmodule ConduitWeb.ArticleControllerTest do
       refute updated_at == ""
     end
   end
+
+  describe "list articles" do
+    setup [
+      :create_author,
+      :publish_articles,
+    ]
+
+    @tag :web
+    test "should return published articles by date published", %{conn: conn} do
+      conn = get conn, article_path(conn, :index)
+      json = json_response(conn, 200)
+      articles = json["articles"]
+      first_created_at = Enum.at(articles, 0)["createdAt"]
+      first_updated_at = Enum.at(articles, 0)["updatedAt"]
+      second_created_at = Enum.at(articles, 1)["createdAt"]
+      second_updated_at = Enum.at(articles, 1)["updatedAt"]
+
+      assert json == %{
+        "articles" => [
+          %{
+            "slug" => "how-to-train-your-dragon-2",
+            "title" => "How to train your dragon 2",
+            "description" => "So toothless",
+            "body" => "It a dragon",
+            "tagList" => ["dragons", "training"],
+            "createdAt" => first_created_at,
+            "updatedAt" => first_updated_at,
+            "favorited" => false,
+            "favoritesCount" => 0,
+            "author" => %{
+              "username" => "jake",
+              "bio" => nil,
+              "image" => nil,
+              "following" => false,
+            }
+          },
+          %{
+            "slug" => "how-to-train-your-dragon",
+            "title" => "How to train your dragon",
+            "description" => "Ever wonder how?",
+            "body" => "You have to believe",
+            "tagList" => ["dragons", "training"],
+            "createdAt" => second_created_at,
+            "updatedAt" => second_updated_at,
+            "favorited" => false,
+            "favoritesCount" => 0,
+            "author" => %{
+              "username" => "jake",
+              "bio" => nil,
+              "image" => nil,
+              "following" => false,
+            }
+          },
+        ],
+        "articlesCount" => 2,
+      }
+    end
+  end
+
+  defp create_author(_context) do
+    {:ok, author} = fixture(:author, user_uuid: UUID.uuid4())
+
+    [
+      author: author,
+    ]
+  end
+
+  defp publish_articles(%{author: author}) do
+    fixture(:article, author: author)
+    fixture(:article, author: author, title: "How to train your dragon 2", description: "So toothless", body: "It a dragon")
+
+    []
+  end
 end
