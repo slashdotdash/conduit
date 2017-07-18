eab5162de1d6f88be5e4ba19e26a9e1dd78148df
Ben Smith <ben@10consulting.com>
2017-07-18 16:03:20 +0100

    Filter by author

--- lib/conduit/blog/queries/list_articles.ex
+++ lib/conduit/blog/queries/list_articles.ex
@@ -5,6 +5,7 @@ defmodule Conduit.Blog.Queries.ListArticles do
 
   defmodule Options do
     defstruct [
+      author: nil,
       limit: 20,
       offset: 0,
     ]
@@ -14,15 +15,17 @@ defmodule Conduit.Blog.Queries.ListArticles do
 
   def paginate(params, repo) do
     options = Options.new(params)
+    query = options |> query()
 
-    articles = query() |> entries(options) |> repo.all()
-    total_count = query() |> count() |> repo.aggregate(:count, :uuid)
+    articles = query |> entries(options) |> repo.all()
+    total_count = query |> count() |> repo.aggregate(:count, :uuid)
 
     {articles, total_count}
   end
 
-  defp query do
+  defp query(options) do
     from(a in Article)
+    |> filter_by_author(options)
   end
 
   defp entries(query, %Options{limit: limit, offset: offset}) do
@@ -34,5 +37,10 @@ defmodule Conduit.Blog.Queries.ListArticles do
 
   defp count(query) do
     query |> select([:uuid])
+  end
+
+  defp filter_by_author(query, %Options{author: nil}), do: query
+  defp filter_by_author(query, %Options{author: author}) do
+    query |> where(author_username: ^author)
   end
 end
