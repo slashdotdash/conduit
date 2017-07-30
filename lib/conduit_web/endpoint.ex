f0877476a600dd84c15c2ff777d22935993c8834
Ben Smith <ben@10consulting.com>
2017-07-30 21:02:01 +0100

    Remove Phoenix pub/sub and default user socket
    Not required for RealWorld REST API.

--- lib/conduit_web/endpoint.ex
+++ lib/conduit_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule ConduitWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :conduit
 
-  socket "/socket", ConduitWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
