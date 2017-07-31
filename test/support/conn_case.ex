160120c771a22d0f48a5d2ceee6201fa8181eb17
Ben Smith <ben@10consulting.com>
2017-07-31 09:23:22 +0100

    Remove Ecto SQL sandbox

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -26,13 +26,7 @@ defmodule ConduitWeb.ConnCase do
     end
   end
 
-
-  setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Conduit.Repo)
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Conduit.Repo, {:shared, self()})
-    end
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
-
 end
