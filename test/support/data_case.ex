47e19c54188d1eff0435f060a9557d78aa4a41b8
Ben Smith <ben@10consulting.com>
2017-07-31 09:23:22 +0100

    Remove Ecto SQL sandbox

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -25,13 +25,7 @@ defmodule Conduit.DataCase do
     end
   end
 
-  setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Conduit.Repo)
-
-    unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Conduit.Repo, {:shared, self()})
-    end
-
+  setup _tags do
     :ok
   end
 
